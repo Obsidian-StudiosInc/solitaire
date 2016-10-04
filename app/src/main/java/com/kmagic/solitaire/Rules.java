da73fe067844058e941d25d8fa4468ac80cb8cf5
William L. Thomson Jr <wlt@o-sinc.com>
2016-10-04 02:08:02 -0400

    app/src/main/java/com/kmagic/solitaire/Rules.java: Removed unused imports

--- app/src/main/java/com/kmagic/solitaire/Rules.java
+++ app/src/main/java/com/kmagic/solitaire/Rules.java
@@ -1,25 +1,23 @@
 /*
-  Copyright 2008 Google Inc.
-  
+  Original Work Copyright 2008-2010 Google Inc.
+  Modified Work Copyright 2016 Obsidian-Studios, Inc.
+
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
-  
+
        http://www.apache.org/licenses/LICENSE-2.0
-  
+
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-*/ 
+*/
 package com.kmagic.solitaire;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.graphics.Canvas;
-import java.lang.InterruptedException;
-import java.lang.Thread;
+
 import java.util.Stack;
 
 
