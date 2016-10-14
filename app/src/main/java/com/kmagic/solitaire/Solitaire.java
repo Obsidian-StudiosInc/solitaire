f217d21633c60a1f48807c5d1efd15424b2165bf
William L. Thomson Jr <wlt@o-sinc.com>
2016-10-14 00:01:36 -0400

    app/src/main/java/com/kmagic/solitaire/Solitaire.java: Removed unused import

--- app/src/main/java/com/kmagic/solitaire/Solitaire.java
+++ app/src/main/java/com/kmagic/solitaire/Solitaire.java
@@ -23,7 +23,6 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 // Base activity class.
