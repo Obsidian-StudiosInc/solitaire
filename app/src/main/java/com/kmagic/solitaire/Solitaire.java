0ce5cff8fde9b18d929a9d67130e6d86326988a9
William L. Thomson Jr <wlt@o-sinc.com>
2016-10-26 17:01:26 -0400

    app/src/main/java/com/kmagic/solitaire/Solitaire.java: Drop landscape
    1. No longer switching to landscape on start, will let phone and
       rotation handle that naturally

--- app/src/main/java/com/kmagic/solitaire/Solitaire.java
+++ app/src/main/java/com/kmagic/solitaire/Solitaire.java
@@ -48,8 +48,6 @@ public class Solitaire extends Activity {
     super.onCreate(savedInstanceState);
     mDoSave = true;
 
-    // Force landscape and no title for extra room
-    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     requestWindowFeature(Window.FEATURE_ACTION_BAR);
     getActionBar().hide();
 
